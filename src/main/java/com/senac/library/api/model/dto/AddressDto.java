7961817610ece0bae7c4a39195d254050bfabeed
Vinicius Duarte Mendes Nepomuceno <vinicius.nepomuceno1@ibm.com>
2021-12-06 22:04:55 -0300

    jacoco

--- src/main/java/com/senac/library/api/model/dto/AddressDto.java
+++ src/main/java/com/senac/library/api/model/dto/AddressDto.java
@@ -1,15 +1,7 @@
 package com.senac.library.api.model.dto;
 
 import com.senac.library.api.model.entities.Address;
-import com.senac.library.api.model.entities.Book;
-import com.senac.library.api.model.entities.TypeValue;
-import lombok.Data;
 
-import javax.persistence.Column;
-import java.time.LocalDate;
-import java.util.List;
-
-@Data
 public class AddressDto {
 
     private Long id;
@@ -32,5 +24,77 @@ public class AddressDto {
         this.country = address.getCountry();
         this.zip = address.getZip();
         this.principal = address.getPrincipal();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public Boolean getPrincipal() {
+        return principal;
+    }
+
+    public void setPrincipal(Boolean principal) {
+        this.principal = principal;
     }
 }
